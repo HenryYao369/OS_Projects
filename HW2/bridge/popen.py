f63ac955bef4c2b43904e6044cb31b21b8fb97fa
HenryYAO <hengzhi369@gmail.com>
2015-08-08 22:13:04 -0400

    HW4: No coding part on File Systems -- will do one by myself later on.

--- HW2/bridge/popen.py
+++ HW2/bridge/popen.py
@@ -3,7 +3,7 @@ from subprocess import Popen
 import sys
 
 for i in xrange(100):
-    # p = Popen([sys.executable,'bridgeMDG_modf.py'])  # let program launch program: cool!!
+    p = Popen([sys.executable,'bridgeMDG_modf.py'])  # let program launch program: cool!!
     p.wait()
 
     print(i)
